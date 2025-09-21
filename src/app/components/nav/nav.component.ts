9ae4b4f28ccdff6557e2de67e9dbeea85dc85cbc
jjas <ayosuajon4@gmail.com>
2025-09-21 20:44:13 -0300

    Avances_Importantes

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
+  constructor
 
+ (private router:Router) {}
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter, Routes, withEnabledBlockingInitialNavigation, withInMemoryScrolling } from '@angular/router';
+import { PresentacionComponent } from './app/components/presentacion/presentacion.component';
+const routes: Routes = [
+  { path: 'presentacion', component: PresentacionComponent },
+  { path: '', redirectTo: '/presentacion', pathMatch: 'full' }
+];
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(
+      routes,
+      withEnabledBlockingInitialNavigation(),
+      withInMemoryScrolling({ anchorScrolling: 'enabled' }) // 👈 scroll automático a fragmentos
+    )
+  ]
+});
