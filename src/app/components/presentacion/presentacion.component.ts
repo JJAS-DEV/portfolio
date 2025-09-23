456a19dad973e42fe942369b032b28514981f0ad
jjas <ayosuajon4@gmail.com>
2025-09-22 23:17:25 -0300

    Proyectos_Terminado

--- src/app/components/presentacion/presentacion.component.ts
+++ src/app/components/presentacion/presentacion.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-presentacion',
@@ -7,6 +9,44 @@ import { Component } from '@angular/core';
   templateUrl: './presentacion.component.html',
   styleUrl: './presentacion.component.css'
 })
-export class PresentacionComponent {
+export class PresentacionComponent implements OnInit {
+
+  constructor(private route: ActivatedRoute, private scroller: ViewportScroller) { }
+
+  ngOnInit(): void {
+    this.route.fragment.subscribe(fragment => {
+      if (fragment) {
+        setTimeout(() => {
+          this.scroller.scrollToAnchor(fragment);
+        }, 100); // espera para que el DOM esté listo
+      }
+    });
+  }
+
+
+
+  fotos: string[] = [
+    'SistemaInventario/SistemaInventario(5).png',
+    'SistemaInventario/SistemaInventario(4).png',
+    'SistemaInventario/sistemaInventario(1).jpeg',
+    'SistemaInventario/SistemaInventario(2).jpeg',
+    'SistemaInventario/SistemaInventario(3).jpeg',
+    'SistemaInventario/SistemaInventario(6).png',
+
+
+  ];
+
+  indiceActual: number = 0;
+
+  siguienteFoto() {
+    this.indiceActual = (this.indiceActual + 1) % this.fotos.length;
+  }
+
+  anteriorFoto() {
+    this.indiceActual =
+      (this.indiceActual - 1 + this.fotos.length) % this.fotos.length;
+  }
+
+
 
 }
